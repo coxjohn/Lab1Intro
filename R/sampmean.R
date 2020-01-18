903c75146b41fe6f06b05d335db1097ffeba9a69
John Cox <john.d.cox-1@ou.edu>
2020-01-18 17:04:28 -0600

    Sample mean function complete; needs documentation

--- R/sampmean.R
+++ R/sampmean.R
@@ -1,3 +1,16 @@
+#' Sample mean
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
 sampmean = function(x){
+  output = vector(mode = "numeric", length = dim(x)[2])
 
+  for(i in 1:dim(x)[2]){
+    output[i] = mean(unlist(x[i]))
+  }
+  return(output)
 }
