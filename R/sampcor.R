3e8f54d0f91607bc46abb9355a974e74fda89780
John Cox <john.d.cox-1@ou.edu>
2020-01-18 18:01:00 -0600

    Sample correlation coefficient function complete; needs varnames fixed and documentation

--- R/sampcor.R
+++ R/sampcor.R
@@ -1,3 +1,23 @@
-sampcor = function(x){
+#' Sample correlation coefficient
+#'
+#' @param x
+#' @param varnames
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sampcor = function(x, varnames = NULL){
+  nrows = dim(x)[1]
+  ncols = dim(x)[2]
+  output = matrix(nrow = ncols, ncol = ncols, dimnames = varnames)
 
+  s = sampcov(x)
+
+  for(i in 1:ncols){
+    for(k in 1:ncols){
+      output[i,k] = s[i,k] / (sqrt(s[i,i]*s[k,k]))
+    }
+  }
+  return(output)
 }
