352c7b3aac1cf2f8e6b1f2f0f06ebd492c56e88b
John Cox <john.d.cox-1@ou.edu>
2020-01-22 00:02:45 -0600

    Added documentation, changed body to use functions that were not mean()

--- R/sampmean.R
+++ R/sampmean.R
@@ -1,16 +1,22 @@
 #' Sample mean
 #'
-#' @param x
+#' Returns a vector of the sample mean
+#'
+#'
+#' Accepts a data set and takes the mean of each variable and then outputs a
+#' vector containing each of those means
+#' @param x a data matrix
 #'
 #' @return
 #' @export
 #'
 #' @examples
+#' sampmean(data)
 sampmean = function(x){
   output = vector(mode = "numeric", length = dim(x)[2])
 
   for(i in 1:dim(x)[2]){
-    output[i] = mean(unlist(x[i]))
+    output[i] = sum(unlist(x[i])) / dim(x[i])[1]
   }
   return(output)
 }
